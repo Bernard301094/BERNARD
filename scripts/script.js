7beef3b42d389639966dd3a4de6bfd4e158acbed
Bernard301094 <bernard30101994@gmail.com>
2024-12-20 15:46:51 -0300

    Seleccionando los elementos del DOM

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const botonNumeros = document.getElementsByName('data-number');
+const botonOpera = document.getElementsByName('data-opera');
+const botonIgual = document.getElementsByName('data-igual')[0];
+const botonDelete = document.getElementsByName('data-delete')[0];
+
+let result = document.querySelector('#result');
+
+console.log(result)
