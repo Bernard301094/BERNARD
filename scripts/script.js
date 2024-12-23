57d2b77a4d76b8865b52985eafe835148522a406
Bernard301094 <bernard30101994@gmail.com>
2024-12-23 14:36:07 -0300

    Funciones programadas

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@ const botonIgual = document.getElementsByName('data-igual')[0];
 const botonDelete = document.getElementsByName('data-delete')[0];
 
 let result = document.querySelector('#result');
+let operacionActual = '';
+let operacionAnterior = '';
+let operacion = undefined;
 
 botonNumeros.forEach(boton => {
     boton.addEventListener('click', function(){
@@ -26,3 +29,57 @@ botonDelete.addEventListener('click', function() {
     clear();
     actualizarDisplay();
 })
+
+function selectOperacion(op) {
+    if(operacionActual === '') return;
+    if(operacionAnterior !== '') {
+        calcular();
+    }
+    operacion = op.toString();
+    operacionAnterior = operacionActual;
+    operacionActual = '';
+}
+
+function calcular() {
+    let calculo;
+    const anterior = parseFloat(operacionAnterior);
+    const actual = parseFloat(operacionActual);
+    if(isNaN(anterior) || isNaN(actual)) return;
+    switch(operacion) {
+        case '+':
+            calculo = anterior + actual;
+            break;
+        case '-':
+            calculo = anterior - actual;
+            break;
+        case 'x':
+            calculo = anterior * actual;
+            break;
+        case '/':
+            calculo = anterior / actual;
+            break;
+        default:
+            return;
+    }
+    operacionActual = calculo;
+    operacion = undefined;
+    operacionAnterior = '';
+    
+}
+
+function agregarNumero(num) {
+    operacionActual = operacionActual.toString() + num.toString();
+    actualizarDisplay();
+}
+
+function clear() {  
+    operacionActual = '';
+    operacionAnterior = '';
+    operacion = undefined;
+}
+
+function actualizarDisplay() {
+    result.value = operacionActual;
+}
+
+clear();
